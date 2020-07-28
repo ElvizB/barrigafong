5cb86b6b5ff8bb463b1b80d681a7c972c4444d91
ElvisB <sagitario12>
2020-07-28 01:59:25 -0500

    update v01

---
+++ app/config.inc.php
@@ -0,0 +1,24 @@
+<?php 
+
+//define('NOMBRE_SERVIDOR', 'localhost');
+//define('NOMBRE_USUARIO', 'root');
+//define('PASSWORD', '');
+//define('NOMBRE_BD', 'andean');
+
+//rutas de la web
+//https://barrigafong.herokuapp.com/
+//http://localhost/fongperu
+
+define("SERVIDOR", "https://barrigafong.herokuapp.com/");
+define("RUTA_NOSOTROS", SERVIDOR."/nosotros");
+define("RUTA_SERVICIOS", SERVIDOR."/servicios");
+define("RUTA_GALERIA", SERVIDOR."/galeria");
+define("RUTA_SOCIAL", SERVIDOR."/proyeccion-social");
+define("RUTA_CONTACTO", SERVIDOR."/contacto");
+//define("RUTA_CATEGORIA", SERVIDOR."/categoria-2007");
+//define("RUTA_CONTACTO", SERVIDOR."/Contacto");
+//define("RUTA_TOQUE", SERVIDOR."/toque-final");
+//recursos
+define("RUTA_CSS", SERVIDOR . "/css/");
+define("RUTA_JS", SERVIDOR . "/js/");
+define("RUTA_IMG", SERVIDOR . "/images/");
---
+++ vistas/home.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'app/config.inc.php';
+include_once 'plantillas/documento-declaracion.inc.php';
+?>
+
+hola
+
+
+<?php
+  include_once 'plantillas/footer.php';
+  include_once 'plantillas/documento-cierre.inc.php';
+?>
